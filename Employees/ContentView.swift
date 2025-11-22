bacacd0331d20ae75e7cc227705287e0e4c75a91
Patricia Martínez Espert <patrimespert@outlook.com>
2025-11-22 22:32:51 +0100

    Initial Commit

---
+++ Employees/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Employees
+//
+//  Created by Patricia M Espert on 22/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Employees/EmployeesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeesApp.swift
+//  Employees
+//
+//  Created by Patricia M Espert on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct EmployeesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
